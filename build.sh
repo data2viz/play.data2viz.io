1ef0cca4d6c7dc29efae154ea49951f4e1d09aef
gzoritchak <g.zoritchak@gmail.com>
2018-10-18 22:30:23 +0200

    Fix gradle task.

--- build.sh
+++ build.sh
@@ -2,6 +2,5 @@
 
 SERVER=$(basename `pwd`)
 
-docker run -u root --rm -v "$PWD":/home/gradle/project -w /home/gradle/project gradle:4.7-jdk8 gradle ::build
-
+docker run -u root --rm -v "$PWD":/home/gradle/project -w /home/gradle/project gradle:4.7-jdk8 gradle build
 docker build -t $SERVER back
