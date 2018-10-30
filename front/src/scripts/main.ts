710e9e97ff2eadb672d35f57237ee6f10e3db9fe
azertypow <azertypow@gmail.com>
2018-10-30 19:40:49 +0100

    feat: set editors initialisation with script

--- front/src/scripts/main.ts
+++ front/src/scripts/main.ts
@@ -1,1 +1,7 @@
 console.log("hello")
+
+declare function KotlinPlayground(selector: string): any
+
+document.addEventListener('DOMContentLoaded', function() {
+    KotlinPlayground('.kotlin-code');
+});
