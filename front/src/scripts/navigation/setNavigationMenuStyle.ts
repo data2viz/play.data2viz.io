075f452e676a5ceaf43aa3142ddc53de6b70a83e
azertypow <azertypow@gmail.com>
2018-11-01 13:29:56 +0100

    chore: extract navigation level one styling in new function

--- front/src/scripts/navigation/setNavigationMenuStyle.ts
+++ front/src/scripts/navigation/setNavigationMenuStyle.ts
@@ -1,11 +1,18 @@
 import {HTML_SELECTORS} from "../HTML_SELECTORS"
 
-const CURRENT_SECTION = "active"
 export function setNavigationMenuStyle() {
-    const level1MenuNavigationElements = document.querySelectorAll(`${HTML_SELECTORS.NAVIGATION_MENU} > li > a`)
+    setLevelNavOne()
+}
+
+function setLevelNavOne() {
+    const CURRENT_SECTION = "active"
+
+    const LEVEL_ON_NAVIGATION_ELEMENTS = document.querySelectorAll(`${HTML_SELECTORS.NAVIGATION_MENU} > li > a`)
+
     const currentTitlePage = document.querySelector("h1")
+
     if(currentTitlePage !== null){
-        for(const level1MenuNavigationElement of level1MenuNavigationElements) {
+        for(const level1MenuNavigationElement of LEVEL_ON_NAVIGATION_ELEMENTS) {
             if(level1MenuNavigationElement.innerHTML === currentTitlePage.innerHTML) {
                 level1MenuNavigationElement.classList.add(CURRENT_SECTION)
             }
