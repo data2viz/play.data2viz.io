4b06ceb57334bb455eeffcd622611a95c9e66204
azertypow <azertypow@gmail.com>
2018-11-08 14:07:26 +0100

    chore: clean navigation ts files

--- front/src/scripts/main.ts
+++ front/src/scripts/main.ts
@@ -1,12 +1,12 @@
-import {menuPositionObserver} from "./menuPositionObserver"
+import {setMenuPosition} from "./navigation/setMenuPosition"
 import {HTML_SELECTORS} from "./HTML_SELECTORS"
-import {setCurrentSectionName} from "./navigationMenu/setCurrentSectionName"
-import {setNavigationMenu} from "./navigationMenu/setNavigationMenu"
+import {setCurrentSectionName} from "./navigation/setCurrentSectionName"
+import {setNavigationMenu} from "./navigation/setNavigationMenu"
 import {setEditors} from "./editor/setEditors"
 
 setEditors()
 
-menuPositionObserver(
+setMenuPosition(
     document.querySelector(HTML_SELECTORS.CONTENT) as HTMLElement,
     document.querySelector(HTML_SELECTORS.TOP_HEADER) as HTMLElement,
 )
